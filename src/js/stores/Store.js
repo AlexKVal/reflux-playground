c9e87dd30b4ea0f9a963e244749196ffa08f9722
AlexKVal <alexkval@gmail.com>
2015-03-16 22:52:08 +0200

    use filltext.com via jsonp

--- src/js/stores/Store.js
+++ src/js/stores/Store.js
@@ -1,11 +1,21 @@
 var Reflux = require('reflux');
-var request = require('superagent');
+// var request = require('superagent');
+var JSONP = require('browser-jsonp');
 
 var Store = Reflux.createStore({
   data: {message: 'Start message'},
+
   init: function () {
-    // request();
+    JSONP({
+      url: 'http://www.filltext.com/',
+      data: { rows: 10, name: '{firstName}' },
+      success: function(data) {
+        console.log(JSON.stringify(data));
+        this.trigger({ message: data[0].name});
+      }.bind(this)
+    });
   },
+
   getInitialState: function() {
     return this.data;
   },
